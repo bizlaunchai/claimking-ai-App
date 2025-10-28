6129ca36a3084736c5fb4fb1aeb83ff7cf5d15ac
developeralamgirbd <developer.alamgirbd@gmail.com>
2025-10-28 12:26:03 +0600

    [Done] sidebar redesign

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -51,17 +51,13 @@ const DashboardLayout = ({ children }) => {
         <div className="flex flex-col flex-1 min-h-screen">
           {/* Header */}
           <header
-              className={`fixed flex-1 top-0 z-40 w-full bg-white shadow-sm border-b border-gray-200 transition-all duration-300
-                                ${isSidebarCollapsed ? "lg:ml-20 lg:pr-20" : "lg:ml-64 lg:pr-64"}`}
+              className={`fixed flex-1 top-0 z-40 w-full bg-white shadow-sm border-b border-gray-200 transition-all duration-300 `}
           >
-            <DashboardHeader title="ClaimKing Dashboard" />
+            {/*<DashboardHeader title="ClaimKing Dashboard" />*/}
           </header>
 
           {/* Page Content */}
-          <main
-              className={`flex-1 overflow-y-auto pt-[80px] p-6 transition-all duration-300
-            ${isSidebarCollapsed ? "lg:ml-20" : "lg:ml-64"}`}
-          >
+          <main>
             {children}
           </main>
         </div>
