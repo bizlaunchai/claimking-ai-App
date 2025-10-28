91e9e44e7c251d1e870dfc6d55661ea348bec15f
developeralamgirbd <developer.alamgirbd@gmail.com>
2025-10-29 00:04:47 +0600

    [Done] client portal header

--- components/layout/AppSidebar.jsx
+++ components/layout/AppSidebar.jsx
@@ -190,14 +190,14 @@ function Sidebar() {
                         </div>
 
                         <div className="nav-item">
-                            <a href="/client-portal" className="nav-link" data-tooltip="Client Portal">
+                            <Link href="/dashboard/client-portal" className={`nav-link ${pathname === '/dashboard/client-portal' ? 'active' : ''}`} data-tooltip="Client Portal">
                                 <span className="nav-icon">
                                     <svg viewBox="0 0 24 24">
                                         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
                                     </svg>
                                 </span>
                                 <span className="nav-text">Client Portal</span>
-                            </a>
+                            </Link>
                         </div>
 
                         <div className="nav-item">
