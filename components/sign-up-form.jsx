a66a7e3b8db62d24871fad020af60939e53d11a2
developeralamgirbd <developer.alamgirbd@gmail.com>
2026-03-29 11:13:17 +0600

    [Done] google auth

--- components/sign-up-form.jsx
+++ components/sign-up-form.jsx
@@ -15,6 +15,7 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import GoogleAuth from "@/components/auth/GoogleAuth.jsx";
 
 export function SignUpForm({ className, ...props }) {
   const [firstName, setFirstName] = useState("");
@@ -142,6 +143,17 @@ export function SignUpForm({ className, ...props }) {
                 <Button type="submit" className="w-full" disabled={isLoading}>
                   {isLoading ? "Creating an account..." : "Sign up"}
                 </Button>
+                <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t" />
+                  </div>
+                  <div className="relative flex justify-center text-xs uppercase">
+                    <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
+                  </div>
+                </div>
+
+                <GoogleAuth/>
+
               </div>
 
               {/* Login Link */}
