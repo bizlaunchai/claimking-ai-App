16b91e5d55c831d25e985806de5e1422eaee7764
developeralamgirbd <developer.alamgirbd@gmail.com>
2026-04-23 17:25:07 +0600

    fix: remove next js profile api

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -33,48 +33,9 @@ const DashboardLayout = ({ children }) => {
               return;
           }
 
-          console.log(data);
-
-
-          const res = await fetch(`/api/profile/${data?.claims?.sub}`);
-          if (!res.ok) {
-              router.push("/auth/login");
-              return;
-          }
-          // debugger
-          const userProfile = await res.json();
-          console.log({userProfile});
-          setUser(userProfile);
-
       })()
 
   }, [])
-
-  /*return (
-      <div className="flex bg-gray-50 text-gray-900 font-sans">
-        {/!* Sidebar *!/}
-        <AppSidebar
-            isCollapsed={isSidebarCollapsed}
-            setIsCollapsed={setIsSidebarCollapsed}
-            user={user}
-        />
-
-        {/!* Main Content *!/}
-        <div className="flex flex-col flex-1 min-h-screen">
-          {/!* Header *!/}
-          {/!*<header
-              className={`fixed flex-1 top-0 z-40 w-full bg-white shadow-sm border-b border-gray-200 transition-all duration-300 `}
-          >
-            <DashboardHeader title="ClaimKing Dashboard" />
-          </header>*!/}
-
-          {/!* Page Content *!/}
-          <main>
-            {children}
-          </main>
-        </div>
-      </div>
-  );*/
 
     return (
         <div className="bg-gray-50 text-gray-900 font-sans min-h-screen">
@@ -89,25 +50,6 @@ const DashboardLayout = ({ children }) => {
 
             {/* Main Content */}
             <div className={ isCollapsed ? 'ml-[68px]': 'ml-[250px]' } >
-                {/*<button*/}
-                {/*    onClick={() => {*/}
-                {/*        setIsCollapsed(true)*/}
-                {/*        setIsMobileOpen(true)*/}
-                {/*    }}*/}
-                {/*    className="fixed top-1 left-0 z-[9999] bg-amber-400 p-2 rounded shadow lg:hidden"*/}
-                {/*>*/}
-                {/*    <svg*/}
-                {/*        width="30"*/}
-                {/*        height="30"*/}
-                {/*        viewBox="0 0 24 24"*/}
-                {/*        fill="none"*/}
-                {/*        stroke="currentColor"*/}
-                {/*        strokeWidth="2"*/}
-                {/*    >*/}
-                {/*        <polyline points="9 6 15 12 9 18" />*/}
-                {/*    </svg>*/}
-                {/*</button>*/}
-                {/* Page Content */}
                 <main className="px-2 overflow-y-auto">
                     {children}
                 </main>
