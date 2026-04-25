863309269f99724d57e0e55c560ac5a7e335b18e
developeralamgirbd <developer.alamgirbd@gmail.com>
2026-04-25 21:03:20 +0600

    [Done] title

--- app/auth/forgot-password/page.jsx
+++ app/auth/forgot-password/page.jsx
@@ -1,5 +1,8 @@
 import { ForgotPasswordForm } from "@/components/forgot-password-form";
-
+export const metadata = {
+    title: "Forgot Password | ClaimKing AI",
+    description: "",
+};
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
