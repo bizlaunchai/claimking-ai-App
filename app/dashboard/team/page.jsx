c9c78921b62e64bb532b892ba91a6c689c994b65
developeralamgirbd <developer.alamgirbd@gmail.com>
2026-05-13 16:25:27 +0600

    [Done] team page design

---
+++ app/dashboard/team/page.jsx
@@ -0,0 +1,12 @@
+import Team from "@/app/dashboard/team/Team.jsx";
+
+export const metadata = {
+    title: "Team Management | ClaimKing AI",
+    description: "Invite teammates, assign roles, and control access",
+};
+
+const Page = () => {
+    return <Team />;
+};
+
+export default Page;
